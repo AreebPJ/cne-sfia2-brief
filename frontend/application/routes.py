cd79d44c8cbb81528d696ea34271a08f79b0298a
AreebPJ <areebpanjwani09@gmail.com>
2020-09-21 12:06:12 +0000

    made some changes to the dockerfiles

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -5,5 +5,5 @@ import requests
 
 @app.route('/', methods=['GET'])
 def get_users():
-    response = requests.get('http://35.246.102.140:5001/backend').json()
+    response = requests.get('http://backend:5001/backend').json()
     return render_template('index.html', title='Home', response=response)
